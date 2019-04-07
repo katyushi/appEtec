27db7823fa8c7452b5aee042d7a3a122ef338d12
katyushi <hugo.luiz@outlook.com>
2019-04-07 20:21:17 -0300

    reset

--- EtecIbitinga/src/app/app.module.ts
+++ EtecIbitinga/src/app/app.module.ts
@@ -8,18 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+      InAppBrowser,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- EtecIbitinga/src/app/home/home.page.ts
+++ EtecIbitinga/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  options: InAppBrowserOptions = {
+    location : 'yes', // Or 'no'
+    hidden : 'no', // Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes', // Android only ,shows browser zoom controls
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', // Android only
+    closebuttoncaption : 'Close', // iOS only
+    disallowoverscroll : 'no', // iOS only
+    toolbar : 'yes', // iOS only
+    enableViewportScale : 'no', // iOS only
+    allowInlineMediaPlayback : 'no', // iOS only
+    presentationstyle : 'pagesheet', // iOS only
+    fullscreen : 'yes', // Windows only
+  };
 
+  constructor(private iab: InAppBrowser) { }
+
+  public openWithInAppBrowser(url: string) {
+    const target = '_blank';
+    this.iab.create(url, target, this.options);
+  }
 }
