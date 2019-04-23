a860eb018c8f1ef3447355c24d6cf73be8f21bdf
hugo lazzari <hugo.luiz@outlook.com>
2019-04-23 11:53:15 -0300

    added button, card and new page

---
+++ etecIbitinga/src/app/fotalunos/fotalunos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fotalunos',
+  templateUrl: './fotalunos.page.html',
+  styleUrls: ['./fotalunos.page.scss'],
+})
+export class FotalunosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
