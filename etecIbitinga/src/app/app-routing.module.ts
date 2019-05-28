1201625970662f83f0a459be5f2f7d40976bfa95
hugo lazzari <hugo.luiz@outlook.com>
2019-05-28 19:30:09 -0300

    added pictures and minor changes

--- etecIbitinga/src/app/app-routing.module.ts
+++ etecIbitinga/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'fotalunos', loadChildren: './fotalunos/fotalunos.module#FotalunosPageModule' },
+  { path: 'cursos', loadChildren: './cursos/cursos.module#CursosPageModule' },
 ];
 
 @NgModule({
