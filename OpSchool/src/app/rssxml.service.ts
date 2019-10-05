968cf067717949e25d3120677871419ed0c881e4
hugo lazzari <hugo.luiz@outlook.com>
2019-10-04 22:00:29 -0300

    formated json with data and minor changes

--- OpSchool/src/app/rssxml.service.ts
+++ OpSchool/src/app/rssxml.service.ts
@@ -1,19 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RssxmlService {
 
-  
 
-  constructor(private http: HttpClient) {  }
+  constructor() {  }
 
-  public getProducts(): Observable<any> {
-    return this._http.get(this.tempPath).map((response: Response) =>
-    <any>response.json())
-    .do(data => console.log(JSON.stringify(data)))
-    .catch(this.handleError);
-    }
 }
